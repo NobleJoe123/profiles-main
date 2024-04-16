96c9ecfb0c5c476ed11c117ba71aa5da3294bda0
Lunar-Ahmed <145129924+Lunar-Ahmed@users.noreply.github.com>
2024-04-16 15:44:45 +0100

    update

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
+
 
 app = Flask(__name__)
 
+app.secret_key = 'key'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL-PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'test'
+
+mysql = MySQL(app)
+
 @app.route('/')
-def index():
-    return render_template('index.html')
+def admin():
+    return render_template('admin.html')
 
-
+@app.route('/teachers')
+def teachers():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM timed")
+    data = cur.fetchall()
+    cur.close()
+    return render_template('teacher.html', data=data)
 
 
 if '__main__' == __name__:
