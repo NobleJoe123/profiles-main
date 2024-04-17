d82a21defae168afbfe39fd157db2e3b22639c7c
Lunar-Ahmed <145129924+Lunar-Ahmed@users.noreply.github.com>
2024-04-18 00:10:53 +0100

    updates

--- app.py
+++ app.py
@@ -15,31 +15,43 @@ mysql = MySQL(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM ecommerce timed")
+    data = cur.fetchall()
+    cur.execute("SELECT COUNT(*) FROM timed")
+    total_count = cur.fetchone()[0] + +1
+    cur.close()
+    return render_template('index.html', data=data, total_count=total_count)
 
 @app.route('/admin')
 def admin():
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM ecommerce")
     data = cur.fetchall()
+    cur.execute("SELECT COUNT(*) FROM ecommerce")
+    total_count = cur.fetchone()[0] + +1
     cur.close()
-    return render_template('admin.html', data=data)
+    return render_template('admin.html', data=data, total_count=total_count)
 
 @app.route('/teacher')
 def teacher():
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM timed")
     data = cur.fetchall()
+    cur.execute("SELECT COUNT(*) FROM timed")
+    total_count = cur.fetchone()[0] + +1
     cur.close()
-    return render_template('teacher.html', data=data)
+    return render_template('teacher.html', data=data, total_count=total_count)
 
 @app.route('/student')
 def student():
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM timed")
     data = cur.fetchall()
+    cur.execute("SELECT COUNT(*) FROM timed")
+    total_count = cur.fetchone()[0] + +1
     cur.close()
-    return render_template('student.html', data=data)
+    return render_template('student.html', data=data, total_count=total_count)
 
 if '__main__' == __name__:
     app.run(debug=True)
